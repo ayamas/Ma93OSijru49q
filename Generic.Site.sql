3b08c2a00a63e0359253d3dc810c992eb159ffc6
ayamas <mr_roccox@hotmail.com>
2014-12-07 20:07:07 +0800

    Update the database again

--- Generic.Site.sql
+++ Generic.Site.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 07, 2014 at 09:26 AM
+-- Generation Time: Dec 07, 2014 at 01:05 PM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -16,11 +16,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Database: `lohan`
---
 
--- --------------------------------------------------------
 
 --
 -- Table structure for table `backup`
@@ -31,7 +27,7 @@ CREATE TABLE IF NOT EXISTS `backup` (
   `backup_name` varchar(255) NOT NULL,
   `backup_location` varchar(255) NOT NULL,
   `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -57,6 +53,13 @@ CREATE TABLE IF NOT EXISTS `optimized` (
 `id` int(1) unsigned NOT NULL,
   `modified_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `optimized`
+--
+
+INSERT INTO `optimized` (`id`, `modified_date`) VALUES
+(1, '2014-12-07 03:53:07');
 
 -- --------------------------------------------------------
 
@@ -84,8 +87,7 @@ CREATE TABLE IF NOT EXISTS `user_accounts` (
   `uacc_date_fail_login_ban` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Time user is banned until due to repeated failed logins',
   `uacc_date_last_login` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `uacc_date_added` datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
-
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -161,14 +163,13 @@ CREATE TABLE IF NOT EXISTS `user_privilege_groups` (
 `upriv_groups_id` smallint(5) unsigned NOT NULL,
   `upriv_groups_ugrp_fk` smallint(5) unsigned NOT NULL DEFAULT '0',
   `upriv_groups_upriv_fk` smallint(5) unsigned NOT NULL DEFAULT '0'
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
 
 --
 -- Dumping data for table `user_privilege_groups`
 --
 
 INSERT INTO `user_privilege_groups` (`upriv_groups_id`, `upriv_groups_ugrp_fk`, `upriv_groups_upriv_fk`) VALUES
-(1, 3, 1),
 (3, 3, 3),
 (4, 3, 4),
 (5, 3, 5),
@@ -178,7 +179,8 @@ INSERT INTO `user_privilege_groups` (`upriv_groups_id`, `upriv_groups_ugrp_fk`, 
 (9, 3, 9),
 (10, 3, 10),
 (11, 3, 11),
-(16, 3, 2);
+(16, 3, 2),
+(17, 3, 1);
 
 -- --------------------------------------------------------
 
@@ -190,24 +192,7 @@ CREATE TABLE IF NOT EXISTS `user_privilege_users` (
 `upriv_users_id` smallint(5) NOT NULL,
   `upriv_users_uacc_fk` int(11) NOT NULL DEFAULT '0',
   `upriv_users_upriv_fk` smallint(5) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
-
---
--- Dumping data for table `user_privilege_users`
---
-
-INSERT INTO `user_privilege_users` (`upriv_users_id`, `upriv_users_uacc_fk`, `upriv_users_upriv_fk`) VALUES
-(1, 1, 1),
-(2, 1, 2),
-(3, 1, 3),
-(4, 1, 4),
-(5, 1, 5),
-(6, 1, 6),
-(7, 1, 7),
-(8, 1, 8),
-(9, 1, 9),
-(10, 1, 10),
-(11, 1, 11);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 -- --------------------------------------------------------
 
@@ -223,14 +208,7 @@ CREATE TABLE IF NOT EXISTS `user_profiles` (
   `upro_last_name` varchar(50) NOT NULL DEFAULT '',
   `upro_phone` varchar(25) NOT NULL DEFAULT '',
   `upro_newsletter` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
-
---
--- Dumping data for table `user_profiles`
---
-
-INSERT INTO `user_profiles` (`upro_id`, `upro_uacc_fk`, `upro_company`, `upro_first_name`, `upro_last_name`, `upro_phone`, `upro_newsletter`) VALUES
-(4, 1, '', 'magister', 'magister', '423432', 0);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Indexes for dumped tables
@@ -304,7 +282,7 @@ ALTER TABLE `user_profiles`
 -- AUTO_INCREMENT for table `backup`
 --
 ALTER TABLE `backup`
-MODIFY `backup_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `backup_id` int(10) unsigned NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `optimized`
 --
@@ -314,7 +292,7 @@ MODIFY `id` int(1) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 -- AUTO_INCREMENT for table `user_accounts`
 --
 ALTER TABLE `user_accounts`
-MODIFY `uacc_id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+MODIFY `uacc_id` int(11) unsigned NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `user_groups`
 --
@@ -329,17 +307,17 @@ MODIFY `upriv_id` smallint(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
 -- AUTO_INCREMENT for table `user_privilege_groups`
 --
 ALTER TABLE `user_privilege_groups`
-MODIFY `upriv_groups_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+MODIFY `upriv_groups_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT for table `user_privilege_users`
 --
 ALTER TABLE `user_privilege_users`
-MODIFY `upriv_users_id` smallint(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=27;
+MODIFY `upriv_users_id` smallint(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT for table `user_profiles`
 --
 ALTER TABLE `user_profiles`
-MODIFY `upro_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+MODIFY `upro_id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
